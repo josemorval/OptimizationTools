c3d88b5d97cd1bafd922c6360f35177ac7021b03
Rayco Sánchez García <raycosanchezgarcia@gmail.com>
2017-03-08 11:20:55 +0100

    + Component for vector pool initializing implemented

---
+++ VectorPoolsInitializer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace OptimizationUtilities {
+  public class VectorPoolsInitializer : MonoBehaviour {
+    [SerializeField]
+    private int vector2PoolSize;
+
+    [SerializeField]
+    private int vector3PoolSize;
+
+    [SerializeField]
+    private int vector4PoolSize;
+
+    void Awake () {
+      if (vector2PoolSize > 0) {
+        Pool<Vector2Container>.Init(vector2PoolSize);
+      }
+      if (vector3PoolSize > 0) {
+        Pool<Vector3Container>.Init(vector3PoolSize);
+      }
+      if (vector4PoolSize > 0) {
+        Pool<Vector4Container>.Init(vector4PoolSize);
+      }
+    }
+  }
+}
