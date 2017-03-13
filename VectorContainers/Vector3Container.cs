e9ec5fbe8775cb6156f8c00a948939aeece1c6e5
Rayco Sánchez García <raycosanchezgarcia@gmail.com>
2017-03-13 12:59:01 +0100

    + Added arithmetic functions to Vector3Container and Vector4Container

--- VectorContainers/Vector3Container.cs
+++ VectorContainers/Vector3Container.cs
@@ -5,5 +5,104 @@ namespace OptimizationUtilities
   public class Vector3Container 
   {
     public Vector3 Vector;
+
+    #region Properties
+
+    public float X
+    {
+      get
+      {
+        return Vector.x;
+      }
+      set
+      {
+        Vector.x = value;
+      }
+    }
+
+    public float Y
+    {
+      get
+      {
+        return Vector.y;
+      }
+      set
+      {
+        Vector.y = value;
+      }
+    }
+
+    #endregion
+
+    #region Assignement Methods
+
+    public void Set (ref Vector3 v) 
+    {
+      MVector.Set(ref Vector, ref v);
+    }
+
+    public void Set (Vector3Container v)
+    {
+      Set(ref v.Vector);
+    }
+
+    #endregion
+
+    #region Add Methods
+
+    public void Add (ref Vector3 v) 
+    {
+      MVector.AddTo(ref Vector, ref v);
+    }
+
+    public void Add (Vector3Container v) 
+    {
+      Add(ref v.Vector);
+    }
+
+    public static Vector3Container operator +(Vector3Container v1, Vector3Container v2)
+    {
+      Vector3Container result = Pool<Vector3Container>.GetTemporalIfFunctionEnabled();
+      MVector.AddTo(ref v1.Vector, ref v2.Vector, ref result.Vector);
+      return result;
+    }
+
+    #endregion
+
+    #region Subtraction Methods
+
+    public void Subtract (ref Vector3 v) 
+    {
+      MVector.RemoveFrom(ref Vector, ref v);
+    }
+
+    public void Subtract (Vector3Container v) 
+    {
+      Subtract(ref v.Vector);
+    }
+
+    public static Vector3Container operator -(Vector3Container v1, Vector3Container v2)
+    {
+      Vector3Container result = Pool<Vector3Container>.GetTemporalIfFunctionEnabled();
+      MVector.RemoveFrom(ref v1.Vector, ref v2.Vector, ref result.Vector);
+      return result;
+    }
+
+    #endregion
+
+    #region Scalar Product Methods
+
+    public void MultiplyBy (float scalar) {
+      MVector.ScalarProduct(ref Vector, scalar);
+    }
+
+    public static Vector3Container operator * (Vector3Container v, float scalar) 
+    {
+      Vector3Container result = Pool<Vector3Container>.GetTemporalIfFunctionEnabled();
+      MVector.ScalarProduct(ref v.Vector, scalar, ref result.Vector);
+      return result;
+    }
+
+    #endregion
   }
 }
--- VectorContainers/Vector4Container.cs
+++ VectorContainers/Vector4Container.cs
@@ -5,5 +5,104 @@ namespace OptimizationUtilities
   public class Vector4Container 
   {
     public Vector4 Vector;
+
+    #region Properties
+
+    public float X
+    {
+      get
+      {
+        return Vector.x;
+      }
+      set
+      {
+        Vector.x = value;
+      }
+    }
+
+    public float Y
+    {
+      get
+      {
+        return Vector.y;
+      }
+      set
+      {
+        Vector.y = value;
+      }
+    }
+
+    #endregion
+
+    #region Assignement Methods
+
+    public void Set (ref Vector4 v) 
+    {
+      MVector.Set(ref Vector, ref v);
+    }
+
+    public void Set (Vector4Container v)
+    {
+      Set(ref v.Vector);
+    }
+
+    #endregion
+
+    #region Add Methods
+
+    public void Add (ref Vector4 v) 
+    {
+      MVector.AddTo(ref Vector, ref v);
+    }
+
+    public void Add (Vector4Container v) 
+    {
+      Add(ref v.Vector);
+    }
+
+    public static Vector4Container operator +(Vector4Container v1, Vector4Container v2)
+    {
+      Vector4Container result = Pool<Vector4Container>.GetTemporalIfFunctionEnabled();
+      MVector.AddTo(ref v1.Vector, ref v2.Vector, ref result.Vector);
+      return result;
+    }
+
+    #endregion
+
+    #region Subtraction Methods
+
+    public void Subtract (ref Vector4 v) 
+    {
+      MVector.RemoveFrom(ref Vector, ref v);
+    }
+
+    public void Subtract (Vector4Container v) 
+    {
+      Subtract(ref v.Vector);
+    }
+
+    public static Vector4Container operator -(Vector4Container v1, Vector4Container v2)
+    {
+      Vector4Container result = Pool<Vector4Container>.GetTemporalIfFunctionEnabled();
+      MVector.RemoveFrom(ref v1.Vector, ref v2.Vector, ref result.Vector);
+      return result;
+    }
+
+    #endregion
+
+    #region Scalar Product Methods
+
+    public void MultiplyBy (float scalar) {
+      MVector.ScalarProduct(ref Vector, scalar);
+    }
+
+    public static Vector4Container operator * (Vector4Container v, float scalar) 
+    {
+      Vector4Container result = Pool<Vector4Container>.GetTemporalIfFunctionEnabled();
+      MVector.ScalarProduct(ref v.Vector, scalar, ref result.Vector);
+      return result;
+    }
+
+    #endregion
   }
 }
